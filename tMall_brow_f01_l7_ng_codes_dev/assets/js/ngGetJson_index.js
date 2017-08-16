d9c01c1310ab9f0b208a7fb081ca3e02de23dba2
firesniper <firesniper@msn.com>
2017-08-16 13:31:13 +0800

    new files

--- tMall_brow_f01_l7_ng_codes_dev/assets/js/ngGetJson_index.js
+++ tMall_brow_f01_l7_ng_codes_dev/assets/js/ngGetJson_index.js
@@ -8,7 +8,6 @@ mdu_root.controller
     {
         var pgp_docSerh = { scm : "malldata" , tbNamesStr : "shoe,overcoat" } ;
 
-        var str_servWholeUri = pgp_docSerh.fnStr_getWholeUri () ;
         /*var fnStr_getEmerUrl = function ( params )
         {
             var str_emerDir    =   params && params.str_emerDir ? 
@@ -18,19 +17,59 @@ mdu_root.controller
             var str_emerUrl = pgp_envState.str_baseUrl + str_emerDir + "/" + str_tb [ 0 ] + ".js" ;
             return str_emerUrl ; 
         } ;*/
-        
-        $http.jsonp
-        ( str_servWholeUri + '&jsonp=JSON_CALLBACK' )
-        .success
-        (
-            function ( json_data )
-            {
-                if ( Object.bol_isNullJson ( json_data ) ) 
+        var getAjax = function ( params )
+        {
+            params.reqInc = !params.reqInc || isNaN ( params.reqInc ) ? 0 : params.reqInc ; 
+            if ( params.reqInc > 5 ) return ;
+            var bol_isEmer          = params.bol_isEmer ? params.bol_isEmer : false ;
+            var str_emerUrl         = params.pgp_docSerh.fnStr_getEmerUrl () ;
+            var str_servWholeUri    = params.pgp_docSerh.fnStr_getWholeUri () ;
+            var reqInc              = params.reqInc  ? params.reqInc : 0 ;
+
+            var str_normalUri = str_servWholeUri + '&jsonp=JSON_CALLBACK' ;
+            var str_servUri = bol_isEmer ? str_emerUrl : str_normalUri ;
+            $http.jsonp
+            ( 
+                // str_servWholeUri + '&jsonp=JSON_CALLBACK' 
+                str_servUri
+            )
+            .success
+            (
+                function ( json_data )
                 {
-                    var str_emerUrl = pgp_docSerh.fnStr_getEmerUrl ()
-                } ;
-                console.log ( "json_data:" , json_data ) ;
-            }
+                    if ( Object.bol_isNullJson ( json_data ) ) 
+                    {
+                        console.log ( "str_emerUrl:" , str_emerUrl ) ;
+                        params.bol_isEmer = true ;
+                        params.reqInc ++ ;
+                        // arguments.callee ( params ) ;
+                    } ;
+                    console.log ( "json_data:" , json_data ) ;
+                }
+            )
+            .error
+            (
+                function ( err )
+                {
+                    // if ( params.reqInc > 5 ) return ;
+
+                    console.log ( "err params:" , params ) ;
+
+                    params.bol_isEmer = true ;
+                    params.reqInc ++ ;
+                    console.log () ;
+                    getAjax ( params ) ;
+
+                }
+            ) ;
+
+        } ;
+        getAjax 
+        ( 
+            {
+                pgp_docSerh : pgp_docSerh ,
+                bol_isEmer  : true 
+            } 
         ) ;
     }
 ) ; 
