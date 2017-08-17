c6a0f2190bb5d26208bf9c843b2d3c9386451afa
firesniper <firesniper@msn.com>
2017-08-17 09:05:54 +0800

    newfiles

--- tMall_brow_f01_l7_ng_codes_dev/assets/js/ngGetJson_index.js
+++ tMall_brow_f01_l7_ng_codes_dev/assets/js/ngGetJson_index.js
@@ -1,11 +1,84 @@
++function ()
+{
 console.log ( "angular:" , angular ) ;
 var mdu_root = angular.module ( "mdu_root" , [] ) ;
 console.log ( "mdu_root:" , mdu_root ) ;
+var fn_getAjax = function ( params )
+{
+    params.reqInc = !params.reqInc || isNaN ( params.reqInc ) ? 0 : params.reqInc ; 
+    if ( params.reqInc > 5 ) return ;
+    var $http               = params.$http ;
+    var $scope              = params.$scope ;
+    var bol_isEmer          = params.bol_isEmer ? params.bol_isEmer : false ;
+    var str_emerUrl         = params.pgp_docSerh.fnStr_getEmerUrl 
+    (
+        {
+            str_emerDir : "/emerData_ng/" 
+        }
+    ) ;
+    var str_servWholeUri    = params.pgp_docSerh.fnStr_getWholeUri () ;
+    var reqInc              = params.reqInc  ? params.reqInc : 0 ;
+    // var fn_cb               = params.fn_cb ;
+
+    var str_normalUri = str_servWholeUri + '&jsonp=JSON_CALLBACK' ;
+    var str_servUri = bol_isEmer ? str_emerUrl : str_normalUri ;
+    $http.jsonp
+    ( 
+        // str_servWholeUri + '&jsonp=JSON_CALLBACK' 
+        str_servUri
+    )
+    .success
+    (
+        function ( json_data )
+
+        {
+            if ( Object.bol_isNullJson ( json_data ) ) 
+            {
+                console.log ( "str_emerUrl:" , str_emerUrl ) ;
+                params.bol_isEmer = true ;
+                params.reqInc ++ ;
+                fn_getAjax ( params ) ;
+            } ;
+            console.log ( "json_data:" , json_data ) ;
+            $scope.json_data = json_data ;
+            // fn_cb ( json_data ) ;
+        }
+    )
+    .error
+    (
+        function ( err )
+        {
+            // if ( params.reqInc > 5 ) return ;
+
+            console.log ( "err params:" , params ) ;
+
+            params.bol_isEmer = true ;
+            params.reqInc ++ ;
+            fn_getAjax ( params ) ;
+
+        }
+    ) ;
+
+} ;
+mdu_root.provider
+(
+    "pvd_http" ,
+    function ()
+    {
+        this.$get = function ()
+        {
+            return {
+                fn_getAjax : fn_getAjax 
+            }
+        } ;
+    }
+) ;
 mdu_root.controller
 (
     "ctr-root" ,
-    function ( $scope , $http )
+    function ( $scope , pvd_http , $http )
     {
+        console.log ( "pvd_http:" , pvd_http ) ;
         var pgp_docSerh = { scm : "malldata" , tbNamesStr : "shoe,overcoat" } ;
 
         /*var fnStr_getEmerUrl = function ( params )
@@ -18,63 +91,12 @@ mdu_root.controller
             return str_emerUrl ; 
         } ;*/
 
-        var getAjax = function ( params )
-        {
-            params.reqInc = !params.reqInc || isNaN ( params.reqInc ) ? 0 : params.reqInc ; 
-            if ( params.reqInc > 5 ) return ;
-            var bol_isEmer          = params.bol_isEmer ? params.bol_isEmer : false ;
-            var str_emerUrl         = params.pgp_docSerh.fnStr_getEmerUrl 
-            (
-                {
-                    str_emerDir : "/emerData_ng/" 
-                }
-            ) ;
-            var str_servWholeUri    = params.pgp_docSerh.fnStr_getWholeUri () ;
-            var reqInc              = params.reqInc  ? params.reqInc : 0 ;
-            // var fn_cb               = params.fn_cb ;
-
-            var str_normalUri = str_servWholeUri + '&jsonp=JSON_CALLBACK' ;
-            var str_servUri = bol_isEmer ? str_emerUrl : str_normalUri ;
-            $http.jsonp
-            ( 
-                // str_servWholeUri + '&jsonp=JSON_CALLBACK' 
-                str_servUri
-            )
-            .success
-            (
-                function ( json_data )
-
-                {
-                    if ( Object.bol_isNullJson ( json_data ) ) 
-                    {
-                        console.log ( "str_emerUrl:" , str_emerUrl ) ;
-                        params.bol_isEmer = true ;
-                        params.reqInc ++ ;
-                        getAjax ( params ) ;
-                    } ;
-                    console.log ( "json_data:" , json_data ) ;
-                    // fn_cb ( json_data ) ;
-                }
-            )
-            .error
-            (
-                function ( err )
-                {
-                    // if ( params.reqInc > 5 ) return ;
-
-                    console.log ( "err params:" , params ) ;
-
-                    params.bol_isEmer = true ;
-                    params.reqInc ++ ;
-                    getAjax ( params ) ;
-
-                }
-            ) ;
-
-        } ;
-        getAjax 
+        
+        pvd_http.fn_getAjax 
         ( 
             {
+                $scope      : $scope ,
+                $http       : $http ,
                 pgp_docSerh : pgp_docSerh ,
                 // bol_isEmer  : false 
             } 
@@ -89,3 +111,5 @@ angular.element ( document ).ready
 
     }
 ) ;
+
+} () ;
